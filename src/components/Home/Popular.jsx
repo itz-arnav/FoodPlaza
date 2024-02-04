609539da7ee597a50aedfef6a2dffd38310f9129
itz-arnav <arnavjha07@gmail.com>
2024-02-04 23:36:18 +0530

    fixes made

--- src/components/Home/Popular.jsx
+++ src/components/Home/Popular.jsx
@@ -16,49 +16,51 @@ const Popular = () => {
         <div className={css.popularTitleSection}>Popular Cuisines</div>
         <div className={css.popularDescription}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
 
-        <ul className={css.popularTabSection}>
-          <li
-            className={`${css.tabItem} ${activeTab === 'Italian' ? css.activeTabItem : ''}`}
-            onClick={() => handleTabClick('Italian')}
-          >
-            Italian
-          </li>
-          <li
-            className={`${css.tabItem} ${activeTab === 'Indian' ? css.activeTabItem : ''}`}
-            onClick={() => handleTabClick('Indian')}
-          >
-            Indian
-          </li>
-          <li
-            className={`${css.tabItem} ${activeTab === 'Mexican' ? css.activeTabItem : ''}`}
-            onClick={() => handleTabClick('Mexican')}
-          >
-            Mexican
-          </li>
-          <li
-            className={`${css.tabItem} ${activeTab === 'Chinese' ? css.activeTabItem : ''}`}
-            onClick={() => handleTabClick('Chinese')}
-          >
-            Chinese
-          </li>
-          <li
-            className={`${css.tabItem} ${activeTab === 'More' ? css.activeTabItem : ''}`}
-            onClick={() => handleTabClick('More')}
-          >
-            More
-          </li>
-        </ul>
+        <div className={css.PopularTabContainer}>
+          <ul className={css.popularTabSection}>
+            <li
+              className={`${css.tabItem} ${activeTab === 'Italian' ? css.activeTabItem : ''}`}
+              onClick={() => handleTabClick('Italian')}
+            >
+              Italian
+            </li>
+            <li
+              className={`${css.tabItem} ${activeTab === 'Indian' ? css.activeTabItem : ''}`}
+              onClick={() => handleTabClick('Indian')}
+            >
+              Indian
+            </li>
+            <li
+              className={`${css.tabItem} ${activeTab === 'Mexican' ? css.activeTabItem : ''}`}
+              onClick={() => handleTabClick('Mexican')}
+            >
+              Mexican
+            </li>
+            <li
+              className={`${css.tabItem} ${activeTab === 'Chinese' ? css.activeTabItem : ''}`}
+              onClick={() => handleTabClick('Chinese')}
+            >
+              Chinese
+            </li>
+            <li
+              className={`${css.tabItem} ${activeTab === 'More' ? css.activeTabItem : ''}`}
+              onClick={() => handleTabClick('More')}
+            >
+              More
+            </li>
+          </ul>
+        </div>
 
         <div className={css.popularDataSection}>
           {data.filter(item => item.type === activeTab).map((item, i) => (
             <div key={i} className={css.popularDataItemContainer}>
-              <img src={item.image} alt={item.name} className={css.popularItemImage}/>
+              <img src={item.image} alt={item.name} className={css.popularItemImage} />
               <div className={css.popularItemTitle}>{item.name}</div>
             </div>
           ))}
         </div>
 
-        <div className={css.knowMoreButton}>Know More</div>
+        <div className={css.knowMoreButton}>Show More</div>
       </div>
     </>
   )
--- src/components/Home/data.js
+++ src/components/Home/data.js
@@ -18,7 +18,26 @@ const items = [
   { image: '/images/image7.png', name: 'Poutine', type: 'More' },
   { image: '/images/image8.png', name: 'Ceviche', type: 'More' },
   { image: '/images/image9.png', name: 'Cannoli', type: 'Italian' },
-  { image: '/images/image10.png', name: 'Croissant', type: 'More' }
+  { image: '/images/image10.png', name: 'Croissant', type: 'More' },
+  // Additional Italian dishes
+  { image: '/images/image1.png', name: 'Pesto Pasta', type: 'Italian' },
+  { image: '/images/image2.png', name: 'Bruschetta', type: 'Italian' },
+  { image: '/images/image3.png', name: 'Focaccia Bread', type: 'Italian' },
+
+  // Additional Indian dishes
+  { image: '/images/image4.png', name: 'Saag Paneer', type: 'Indian' },
+  { image: '/images/image5.png', name: 'Biryani', type: 'Indian' },
+  { image: '/images/image6.png', name: 'Samosa', type: 'Indian' },
+
+  // Additional Mexican dishes
+  { image: '/images/image7.png', name: 'Chilaquiles', type: 'Mexican' },
+  { image: '/images/image8.png', name: 'Pozole', type: 'Mexican' },
+  { image: '/images/image9.png', name: 'Guacamole', type: 'Mexican' },
+
+  // Additional Chinese dishes
+  { image: '/images/image10.png', name: 'Kung Pao Chicken', type: 'Chinese' },
+  { image: '/images/image1.png', name: 'Hot Pot', type: 'Chinese' },
+  { image: '/images/image2.png', name: 'Spring Rolls', type: 'Chinese' }
 ];
 
 export default items;
